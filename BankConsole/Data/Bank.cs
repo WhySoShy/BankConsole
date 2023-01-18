acfc2a1a43b0d9cb1a01adcc73bc3895e9611891
Emil Paaske Kvandal <emil.kvandal@gmail.com>
2023-01-18 13:27:04 +0100

    Add project files.

---
+++ BankConsole/Data/Bank.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankConsole.Models;
+
+namespace BankConsole.Data
+{
+    public class Bank
+    {
+        public string BankName { get; } = "Et eller andet bank navn";
+        public List<Account> AccountList { get; set; } = new List<Account>();
+
+        public Account CreateAccount(string name)
+        {
+            if (AccountList.Any(x => x.Name.ToLower() == name.ToLower()))
+                throw new Exception();
+            Account account = new()
+            {
+                Balance = 0,
+                AccountID = AccountList.Count + 1,
+                Name = name,
+            };
+            AccountList.Add(account);
+
+            return account;
+        }
+
+        public decimal Deposit(decimal amount, int accountID) =>
+            AccountList.Find(x => x.AccountID == accountID).Balance += amount;
+
+
+        public decimal Withdraw(decimal amount, int accountID) =>
+            AccountList.Find(x => x.AccountID == accountID).Balance -= amount;
+
+        /// <returns>Account Balance</returns>
+        public decimal Balance()
+        {
+            decimal allBalance = 0;
+            foreach (var item in AccountList)
+                allBalance += item.Balance;
+            return allBalance;
+        }
+    }
+}
---
+++ BankConsole/Program.cs
@@ -0,0 +1,123 @@
+﻿using BankConsole.Data;
+using BankConsole.Models;
+
+
+///TODO:
+/// * FIX SÅ DEN IKKE CRASHER NÅR DU TRYKKER ENTER
+/// 
+
+
+Menu();
+
+static void CreateMenu(int accountID)
+{
+    Bank bank = new();
+    Dictionary<char, string> menuItems = new Dictionary<char, string>();
+    menuItems.Add('A', "Opret konto");
+    menuItems.Add('B', "Indsæt");
+    menuItems.Add('C', "Hæv");  
+    menuItems.Add('D', "Vis saldo");
+    menuItems.Add('E', "Vis bank");
+    menuItems.Add('X', "Afslut");
+
+    Console.Clear();
+    Console.WriteLine($"{bank.BankName} | Logget på som: {bank.AccountList.Find(x => x.AccountID == accountID).Name}");
+    for (int i = 0; i < bank.BankName.Length; i++)
+        Console.Write("-");
+    Console.WriteLine("\n");
+    foreach(var item in menuItems)
+        Console.WriteLine($"{item.Key}: {item.Value}");
+}
+
+static decimal CheckForDecimal(string amount, int accountID)
+{
+    if (Convert.ToDecimal(amount) <= 0)
+    {
+        Console.WriteLine($"Du kan ikke hæve / indsætte {amount}");
+        return Convert.ToDecimal(amount);
+    }
+    while (!decimal.TryParse(amount, out decimal value))
+    {
+        CreateMenu(accountID);
+        Console.WriteLine("\nIkke et decimal \n");
+        Console.Write("Indsæt: ");
+        amount = Console.ReadLine();
+    }
+    return Convert.ToDecimal(amount);
+}
+
+static void Menu()
+{
+    Bank bank = new();
+    Account account = new();
+    decimal amount = 0;
+    int loggedInAs;
+ 
+    
+    Console.Write($"Opret en bruger: ");
+    bank.CreateAccount(Console.ReadLine());
+
+    Console.Write("Indtast dit navn: ");
+    string name = Console.ReadLine();
+    while (!bank.AccountList.Any(x => x.Name.ToLower() == name.ToLower()))
+    {
+        Console.WriteLine("Det blev ikke fundet nogen bruger med dette navn");
+        Console.Write("Indtast dit navn: ");
+        name = Console.ReadLine();
+    }
+    loggedInAs = bank.AccountList.Find(x => x.Name == name.ToLower()).AccountID;
+
+    while (true)
+    {
+        CreateMenu(loggedInAs);
+
+
+        var selectedItem = Console.ReadKey(true);
+
+        switch (selectedItem.Key)
+        {
+            case ConsoleKey.A:
+                Console.Write("Opret bruger: ");
+                account = bank.CreateAccount(Console.ReadLine());
+                break;
+            case ConsoleKey.B:
+                Console.Write("Indtast hvor meget du vil indsætte: ");
+                amount = CheckForDecimal(Console.ReadLine(), loggedInAs);
+                if (amount > 0)
+                Console.WriteLine($"Du har indsat {amount}kr | Ny saldo {bank.Deposit(amount, loggedInAs)}kr");
+
+                break;
+            case ConsoleKey.C:
+                Console.Write("Indtast hvor meget du vil hæve: ");
+                amount = CheckForDecimal(Console.ReadLine(), loggedInAs);
+                
+                if (account.Balance - amount >= 0)
+                {
+                    bank.Withdraw(amount, loggedInAs);
+                    break;
+                }
+                Console.WriteLine($"Du mangler {amount - account.Balance}kr for at kunne trække dette antal ud! | Saldo {account.Balance}kr");
+                break;
+            case ConsoleKey.D:
+                Console.WriteLine($"Nuværende saldo: {bank.AccountList.Find(x => x.Name.ToLower() == name.ToLower()).Balance}kr");
+                break;
+            case ConsoleKey.E:
+                Console.WriteLine(bank.BankName);
+                Console.WriteLine($"Der er {bank.Balance}kr i hele banken");
+                break;
+            case ConsoleKey.X:
+                break;
+
+            default:
+                break;
+        }
+
+
+        Console.WriteLine("Tryk på en knap for at gå vidre" + Console.ReadLine());
+    }
+}
+
+
+
+
+
